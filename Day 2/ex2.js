d1a8053a4916ea56f16ff525bc795f006737e8b0
Jason <j.hart7614@gmail.com>
2018-04-10 16:03:12 +0100

    New Commit

---
+++ Day 2/ex2.js
@@ -0,0 +1,5 @@
+var number = 12
+var name = "john"
+window.alert(name)
+console.log(number)
+document.write(number)
---
+++ Day 2/myscript.js
@@ -0,0 +1,16 @@
+function Squaring(num) {
+    return num * num;
+}
+
+function Sum(a, b, c) {
+    return a + b + c;
+}
+
+var person = {
+
+    name: "Jack",
+    age: 10,
+    occupation: "Chef"
+}
+
+window.alert (person);
