935be5a8d3fd10cc129ac87d279290daa77fd717
Jason <j.hart7614@gmail.com>
2018-04-13 13:37:26 +0100

    last commit

--- Ex Book/Level1Basic.js
+++ Ex Book/Level1Basic.js
@@ -70,4 +70,111 @@ Iteration3()
 
 function Iteration4() {
 
+    var a = document.getElementById("EnterNumber").value;
+
+    var count= 0
+    while (a != 1) {
+        if (a % 3 == 0) {
+            a = a/3;
+            console.log(a);
+            count++
+        }
+        else if ((a+1) % 3 == 0) {
+
+            a = a + 1;
+            console.log(a);
+            count ++
+        }
+
+        else {
+
+            a = a - 1;
+            console.log(a);
+            count++
+        }
+
+    }
+    console.log("number of Iterations: " + count)
 }
+
+function Strings4() {
+
+    var b = document.getElementById("EnterString").value;
+
+    c=0;
+    for (i=0; i < b.length; i++) {
+
+        if (b.charAt(i) == (b.charAt(i+1)) && b.charAt(i) == (b.charAt(i+2))) {
+
+          c  =c + 1;
+
+        }
+    }
+    console.log(c);
+}
+
+
+function Ptag() {
+
+    Deletetext();
+
+    var p = document.createElement("p");
+    p.setAttribute("id", "para");
+    document.getElementsByTagName('body')[0].appendChild(p);
+
+}
+
+function Addtext() {
+
+    var text = document.getElementById("EnterText").value;
+
+    if (document.getElementById("para") !=null) {
+        document.getElementById("para").innerHTML = text;
+    }
+
+}
+
+function Deletetext() {
+
+    if (document.getElementById("para") !=null) {
+
+        document.getElementById("para").remove();
+    }
+
+}
+
+var requestURL = 'https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json';
+
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function() {
+
+    var superHeroes = request.response;
+
+    console.log("Our hero squad is named: " + superHeroes.squadName);
+
+    console.log("One of our members is the " + superHeroes.members[1].name);
+
+}
+
+var requestURL1 = 'https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json';
+
+var request1 = new XMLHttpRequest();
+
+request1.open('GET', requestURL1);
+
+request1.responseType = 'json';
+
+request1.send();
+
+request.onload = function() {
+
+}
+
+
